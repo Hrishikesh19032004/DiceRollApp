f6ef14f1f96d623ef2e4456e1bd8cc5a3c277023
Hrishikesh Dhuri <121174859+Hrishikesh19032004@users.noreply.github.com>
2024-09-27 23:36:50 +0530

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
+void main() {
+  runApp(
+     const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(Color.fromARGB(255,26,2,80),Color.fromARGB(255, 45,7,98)) ,
+      ),
+    ),
+  );
+}
+
